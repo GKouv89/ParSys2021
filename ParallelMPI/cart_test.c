b5068f48e1bb83baf6a11bc2def1c953d7168398
GKouv89 <gina101@windowslive.com>
2021-02-18 12:43:07 +0200

    What part of [-1, 1]x[-1, 1] will each process solve in?

--- ParallelMPI/cart_test.c
+++ ParallelMPI/cart_test.c
@@ -13,14 +13,43 @@ int main(int argc, char* argv[]){
 	MPI_Comm new_comm;
 	MPI_Cart_create(MPI_COMM_WORLD, 2, dim_size, periods, 1, &new_comm);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_world_rank);
-  if(my_world_rank == 0 && comm_size != 80){
-    int i, j, coords[2], curr_rank;
-    for(i = 0; i < sqrt(comm_size); i++){
-      for(j = 0; j < sqrt(comm_size); j++){
-        coords[0] = i;
-        coords[1] = j;
-        MPI_Cart_rank(new_comm, coords, &curr_rank);
-        printf("Process with cart. coords (%d, %d) is %d in the world comm.\n", coords[0], coords[1], curr_rank);
+  /* A test to see how cartesian and world communicator coordinates match */
+  // if(my_world_rank == 0 && comm_size != 80){
+    // int i, j, coords[2], curr_rank;
+    // for(i = 0; i < sqrt(comm_size); i++){
+      // for(j = 0; j < sqrt(comm_size); j++){
+        // coords[0] = i;
+        // coords[1] = j;
+        // MPI_Cart_rank(new_comm, coords, &curr_rank);
+        // printf("Process with cart. coords (%d, %d) is %d in the world comm.\n", coords[0], coords[1], curr_rank);
+      // }
+    // }
+  // }
+  /*************************************************************************/
+  double xRight = 1.0;
+  double xLeft = -1.0;
+  double yBottom = -1.0;
+  double yUp = 1.0;
+  
+  if(comm_size != 80){
+    root = sqrt(comm_size);
+    length = xRight - xLeft;
+    double xLeft_local, xRight_local;
+    double yBottom_local, yUp_local;
+    if(my_world_rank == 0){
+      int i, j, coords[2], curr_rank;
+      for(i = 0; i < sqrt(comm_size); i++){
+        for(j = 0; j < sqrt(comm_size); j++){
+          coords[0] = i;
+          coords[1] = j;
+          MPI_Cart_rank(new_comm, coords, &curr_rank);
+          xLeft_local = xLeft + ((double)coords[0])*(length/root);
+          xRight_local = xLeft_local + (length/root);          
+          yUp_local = yUp - ((double)coords[1])*(length/root);
+          yBottom_local = yUp_local - (length/root);
+          printf("Process with cart. coords (%d, %d) is %d in the world comm.\n", coords[0], coords[1], curr_rank);
+          printf("This process will solve in [%.5lf, %.5lf] x [%.5lf, %.5lf].\n", xLeft_local, xRight_local, yBottom_local, yUp_local);
+        }
       }
     }
   }
